3efa16b766561f794f2ea31020b310e8216b7194
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-14 23:09:41 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,11 +10,10 @@ class StudentsController < ApplicationController
   
   def activate 
     # setting a default value- to false 
-    # @student = Student.find(params[:id])
-    # @student.active = !@student.active
-    # @student.save
-    @student.update(active: !@student.active)
-    redirect_to student_path(@student)
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    
     redirect_to student_path
   end 
 
