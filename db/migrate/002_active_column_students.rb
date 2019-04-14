c43a25b5a8c36ac628bc539f1121e1e735f9fc05
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-14 23:14:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_active_column_students.rb
+++ db/migrate/002_active_column_students.rb
@@ -1,5 +1,5 @@
 class ActiveColumnStudents < ActiveRecord::Migration
   def change
-    add_column :students, :active, :boolean, default: false
+    add_column :students, :active, :boolean, default: rue
   end
 end 
